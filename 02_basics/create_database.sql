ebc40af88b34ae69f5b96f1205bd5fd07dbe581a
Mokadi Surya Prasad <90204775+mokadi-suryaprasad@users.noreply.github.com>
2025-12-03 13:29:19 +0530

    Update create_database.sql

--- 02_basics/create_database.sql
+++ 02_basics/create_database.sql
@@ -1,1 +1,12 @@
--- Create Database
+-- Create Database 
+-- Create a new database
+CREATE DATABASE movies_db;
+
+-- Show all databases
+SHOW DATABASES;
+
+-- Select the database to use
+USE movies_db;
+
+-- Check which database is currently active
+SELECT DATABASE();
